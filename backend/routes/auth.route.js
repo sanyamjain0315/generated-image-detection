e0f07c875237936d90dacb41ccf9119453597c5f
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2025-02-04 13:42:49 +0530

    Added more routes , resetpassword, check auth, forgot-password,verify email

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,11 +2,20 @@ import express from "express";
 import { signup } from "../controllers/auth.contoller.js";
 import { login } from "../controllers/auth.contoller.js";
 import { logout } from "../controllers/auth.contoller.js";
+import {verifyEmail} from "../controllers/auth.contoller.js";
+import {forgotPassword} from "../controllers/auth.contoller.js";
+import {resetPassword} from "../controllers/auth.contoller.js";
+import {checkAuth} from "../controllers/auth.contoller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router= express.Router();
 
+router.get("/check-auth",verifyToken,checkAuth)
 router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
+router.post("/verify-email",verifyEmail)
+router.post("/forgot-password",forgotPassword);
+router.post("/reset-password/:token",resetPassword);
 
 export default router;
