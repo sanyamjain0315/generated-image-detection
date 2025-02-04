968e52d6ba7427f1a2e5d282d67bc38074c6da48
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2025-02-04 13:42:06 +0530

    Added the middleware for checking the Jwt Token verification when user refresh the page

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+export const verifyToken=(req,res,next)=>{
+    const token = req.cookies.token
+    // console.log("token:-",token)
+    if(!token){
+        return res.status(401).json({success:false,message:"Unauthorized - no token provided"});
+    }    
+    try{
+const decoded =  jwt.verify(token,process.env.JWT_SECRET)
+// console.log("decode token :-",decoded)
+if(!decoded.UserId){
+    return res.status(401).json({success:false,message:"Unauthorized - Invalid token"});
+}
+req.userID=decoded.UserId;
+console.log(req.userID)
+next();
+}
+    catch(error){
+    console.log("Error in verification",error);
+    return res.status(500).json({success:false,message:"Server error"});
+    
+    }
+}
