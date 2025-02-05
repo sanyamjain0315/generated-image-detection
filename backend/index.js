a42eaebe49d5bb9d8d38af49d5bfee40340fb049
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2025-02-05 16:57:18 +0530

    added the cors origin for running frontend and backend both on the 5000 port

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,33 @@ import { connectDB } from './db/connectDB.js';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.route.js";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 
 const app = express();
 const PORT =  process.env.PORT || 5000;
-
-app.get("/",(req,res)=>{
-    res.send("Hello world");
-})
+const __dirname=path.resolve();
+const __filename = fileURLToPath(import.meta.url);
 
 app.use(express.json()) // Allow us to parse the incoming Requests with Json payloads req.body
 app.use(cookieParser()); //Allows us to parse the incoming cookies
+app.use(cors({origin:"http://localhost:5173",credentials:true}));
 app.use("/api/auth",authRoutes)
+
+const frontendPath = path.join(__dirname, "./frontend/UI/dist");
+if(process.env.NODE_ENV==="production"){
+  
+    app.use(express.static(frontendPath));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
+    });
+}
 
 app.listen(PORT,()=>{
     connectDB();
-    console.log("Server is running");
+    console.log("Server is running on port",PORT);
 
 })
 
